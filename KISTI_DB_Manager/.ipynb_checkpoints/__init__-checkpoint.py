b186f051448ee93d5a1a263161c63c5cf50ef585
kimyoungjin06 <kimyoungjin06@gmail.com>
2024-02-06 20:06:58 +0900

    Update the README.md

--- KISTI_DB_Manager/.ipynb_checkpoints/__init__-checkpoint.py
+++ KISTI_DB_Manager/.ipynb_checkpoints/__init__-checkpoint.py
@@ -3,13 +3,13 @@
 Note
 ----
 Made by Young Jin Kim (kimyoungjin06@gmail.com)
-Last Update: 2024.02.05, YJ Kim
+Last Update: 2024.02.06, YJ Kim
 
 MariaDB/MySQL Handling for All type DB
 To preprocess, import, export and manage the DB
 """
 
-__version__ = "0.1.0"
+__version__ = "0.3.0"
 
 from . import manage
 from . import preview
